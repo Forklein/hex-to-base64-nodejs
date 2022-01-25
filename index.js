860f3e2e9d99e793e3a89066c221672dd0040384
Forklein <giuseppepisani2@gmail.com>
2022-01-25 14:21:15 +0100

    Add custom function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const hexStr = '4a1b48d7322394a78326cd283f0834093e65421a7e98e90e5b452d160b5d07d8';
-const base64 = Buffer.from(hexStr, 'hex').toString('base64');
+// const base64 = Buffer.from(hexStr, 'hex').toString('base64');
 
-console.log(base64);
+// console.log(base64);
+
+const hexToBase64 = (hexstring) => {
+    return btoa(hexstring.match(/\w{2}/g).map(function (a) {
+        return String.fromCharCode(parseInt(a, 16));
+    }).join(""));
+}
+
+console.log(hexToBase64(hexStr));
+
+
+
